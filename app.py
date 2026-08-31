3422be5a0518d24507c5a4448de4126e34984158
sai <root@ip-172-31-10-248.ap-south-1.compute.internal>
2026-08-31 09:33:30 +0000

    app.py data added

---
+++ app.py
@@ -0,0 +1,38 @@
+# Variables
+name = "Alex"
+age = 20
+
+print(name)
+print(age)
+
+# User input
+name = input("What is your name? ")
+print("Hello,", name)
+
+# Simple math
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# If/else
+age = 18
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are under 18")
+
+# Loop
+for i in range(5):
+    print(i)
+
+# Function
+def greet(name):
+    print("Hello,", name)
+
+greet("Alex")
+
